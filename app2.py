3b10e354cee2101fa4d4ed194b710439b240ee78
1rov <101921388+1rov@users.noreply.github.com>
2022-03-23 17:14:04 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -120,6 +120,7 @@ elif option =='Coba Altair':
     cbgalt = alt.Chart(df_a_prov).mark_bar(tooltip=True).encode(
             x='jml',
             y='provinsi',
+            color='provinsi',
             tooltip=[
                 alt.Tooltip("provinsi", title="Provinsi"),
                 alt.Tooltip("jml", title="Jumlah"),
